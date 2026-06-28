2953be0c4ab00bda954de2292ec70b28878d6c18
Lilly <83720082+LOLthecool@users.noreply.github.com>
2026-06-28 21:13:25 +0100

    avatar collider config marker

--- toolkit/nodes/markers/avatar_collider_config.gd
+++ toolkit/nodes/markers/avatar_collider_config.gd
@@ -1,1 +1,40 @@
+@tool
 extends CCKMarker
+class_name AvatarColliderConfig
+
+@export var radius:float = 0.5
+@export var height:float = 1.0
+
+func _process(delta: float) -> void:
+	var target:Node = get_parent()
+	while target is not Node3D:
+		if get_parent() == null:
+			return
+		target = get_parent()
+	
+	var position:Vector3 = (target as Node3D).global_position
+	position.y += height / 2
+	
+	DebugDraw3D.draw_capsule(
+			position, Quaternion.IDENTITY, radius, height)
+
+func get_uploader_warnings() -> Array[BaseRoot.Warning]:
+	var warnings:Array[BaseRoot.Warning] = get_universal_warnings()
+	
+	return warnings
+
+func prep_for_upload() -> bool:
+	var meta_values:Dictionary[String, Variant] = {}
+	
+	meta_values["marker_version"] = get_marker_version_string()
+	meta_values["radius"] = radius
+	meta_values["height"] = height
+	
+	get_parent().set_meta("AvatarColliderConfig", meta_values)
+	
+	queue_free()
+	
+	return true
+
+func get_marker_version_string() -> String:
+	return "1"
