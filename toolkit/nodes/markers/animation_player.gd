062b0739d03f33a5abc23a4c764e4897e33c71bf
LOLthecool <83720082+LOLthecool@users.noreply.github.com>
2026-07-05 09:22:13 +0100

    its something

--- toolkit/nodes/markers/animation_player.gd
+++ toolkit/nodes/markers/animation_player.gd
@@ -2,16 +2,24 @@
 extends CCKMarker
 class_name CCKAnimationPlayer
 
-@export var animation:Animation
+@export_enum(" ") var animation:String
 @export var active:bool = true
-@export_range(-5, 5, 0.1) var playback_speed:float = 0
+@export_range(-5, 5, 0.1) var playback_speed:float = 1.0
+
+func _validate_property(property: Dictionary) -> void:
+	if property.name == "animation":
+		if get_child(0) is AnimationPlayer:
+			property.hint = PROPERTY_HINT_ENUM
+			property.hint_string = get_child(0).get_animation_list()
 
 func prep_for_upload() -> bool:
+	if get_child(0) is not AnimationPlayer:
+		return false
 	var values:Dictionary[String, Variant] = {}
 	
 	values["marker_version"] = get_marker_version_string()
 	
-	values["animation"] = animation
+	values["animation"] = get_child(0).get_animation(animation)
 	values["active"] = active
 	values["playerback_speed"] = playback_speed
 	
@@ -27,6 +35,30 @@ func get_marker_version_string() -> String:
 func get_uploader_warnings() -> Array[BaseRoot.Warning]:
 	var warnings:Array[BaseRoot.Warning] = get_universal_warnings()
 	
-	# check animation for property tracks or method call tracks
+	if get_child(0) is not AnimationPlayer:
+		warnings.append(BaseRoot.Warning.new(BaseRoot.Warning.WarningLevel.Warning, 
+				"CCKAnimationPlayer needs AnimationPlayer child", 
+				"the player must be the first child of the CCKAnimationPlayer.", 
+				self, false))
+		return warnings
+	
+	var animation:Animation = get_child(0).get_animation(animation)
+	
+	for i in range(0, animation.get_track_count()):
+		if animation.track_get_type(i) == Animation.TrackType.TYPE_METHOD:
+			warnings.append(BaseRoot.Warning.new(BaseRoot.Warning.WarningLevel.Error, 
+					"Method call animations not supported", 
+					"animations are not allowed to call methods", 
+					self, false))
+		if animation.track_get_type(i) == Animation.TrackType.TYPE_VALUE:
+			warnings.append(BaseRoot.Warning.new(BaseRoot.Warning.WarningLevel.Error, 
+					"Property animations not supported", 
+					"this may be supported in the future", 
+					self, false))
+		if animation.track_get_type(i) == Animation.TrackType.TYPE_ANIMATION:
+			warnings.append(BaseRoot.Warning.new(BaseRoot.Warning.WarningLevel.Error, 
+					"Sub animations not supported", 
+					"this may be supported in the future", 
+					self, false))
 	
 	return warnings
